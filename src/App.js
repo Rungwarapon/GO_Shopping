48adac90b801f8572269fb54fab99ea453de0ddc
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2021-04-09 00:12:48 +0700

    start project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="App">
+      <p className="text-center font-italic">HELLO WORLD</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="App">
+        <p className="text-center font-italic">HELLO WORLD</p>
+        <button></button>
+      </div>
+    );
+  }
+  
+  export default App;
+  
