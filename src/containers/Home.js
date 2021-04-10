26823161ded0c8d9aca05926a58e754dadc9f586
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2021-04-10 15:39:53 +0700

    test home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,26 @@
+import { Card, Button } from "react-bootstrap";
 import Nav from "../navbar/navbar"
+import '../styles/App.css';
+
 function Home() {
     return (
       <div className="App">
         <Nav/>
-        <div className="container">
-          <p className="text-center font-italic">Home page wait database  show product</p>
-        </div>
+        <div className="layoutPage">
+          <div>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="/images/test.jpeg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+      </div>
       </div>
     );
   }
