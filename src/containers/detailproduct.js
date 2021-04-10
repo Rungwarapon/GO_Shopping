238db4aa2d786676d2eaaf6a65e7ba854680f4a6
thitiwut088 <thitiwut00897@gmail.com>
2021-04-10 17:05:09 +0700

    detailproduct

--- src/containers/detailproduct.js
+++ src/containers/detailproduct.js
@@ -1,14 +1,42 @@
 import Nav from "../navbar/navbar"
-function detail() {
+import {Button} from 'react-bootstrap';
+import '../styles/App.css';
+function detailproduct() {
     return (
       <div className="App">
         <Nav/>
         <div className="container">
-            <p className="text-center font-italic">detail page wait database show detailproduct</p>
+            <h1 className="text-center font-italic">Product name</h1>
+            <div className="containerProfile">
+              <div className="itemProfile1">
+                <img src="/images/test.jpeg"></img>
+                <div>
+                  <p>name factory</p>
+                </div>
+                <div>
+                  <p>Phone</p>
+                  <p>Mail</p>
+                  <p>Line</p>
+                  <p>Address</p>
+                </div>
+              </div>
+              <div className="itemProfile2">
+                <div>
+                  <p>detail</p>
+                  <p>ราคาจำนวน</p>
+                  <p>Phone number : 0860606060</p>
+                </div>
+                <Button variant="primary">
+                   preorder
+                </Button>
+              </div>
+            </div>
         </div>
+        
+
       </div>
     );
   }
   
-  export default detail;
+  export default detailproduct;
   
