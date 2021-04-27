d8b9c9357a6b5b497b6f1cd099fbce88c911e452
Natchaya Loratanachaiyong <42941107+61070053@users.noreply.github.com>
2021-04-27 20:12:39 +0700

    Edit CompanyManage and AddProduct

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,52 +1,59 @@
 import { Card, Button } from "react-bootstrap";
-import Nav from "../navbar/navbar"
-import '../styles/App.css';
-import Axios from 'axios';
+import Nav from "../navbar/navbar";
+import "../styles/App.css";
+import Axios from "axios";
 import React, { useState, useEffect } from "react";
 
 function Home() {
-    const [Product, SetProduct] = useState([])
-    const [ID, SetID] = useState()
-    const fetchData =()=> {
-      Axios.get("http://localhost:3001/api/products").then(
-          res => { 
-            SetProduct(res.data)
+  const [Product, SetProduct] = useState([]);
+  const [ID, SetID] = useState();
+  const fetchData = () => {
+    Axios.get("http://localhost:3001/api/products").then((res) => {
+      SetProduct(res.data);
+    });
+  };
 
-          })
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-      fetchData();
-    }, []);
-
-    return (
-      <div className="App">
-        <Nav/>
-        <div className="container">
-            <h1 className="text-center font-italic">Product</h1>
-            <div class="card-columns">
-            { Product.map(item =>
-                  <div class="card">
-                    <img class="card-img-top" src="/images/test.jpeg" alt="Card image cap" style={{ width: '100%', height: 'Auto'}}/>
-                    <div class="card-body">
-                      <h5 class="card-title">{item.productName}</h5>
-                      {/* <p class="card-text">{item.detail}</p> */}
-                    </div>
-                    <div class="card-footer">
-                      <Button variant="primary" href={'/detailProduct/'+ item._id}>Detail</Button>
-                    </div>
-                  </div>
-            )}
+  return (
+    <div className="App">
+      <Nav />
+      <div className="container">
+        <h1 className="text-center font-italic">Product</h1>
+        <div class="card-columns">
+          {Product.map((item) => (
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="/images/test.jpeg"
+                alt="Card image cap"
+                style={{ width: "100%", height: "Auto" }}
+              />
+              <div class="card-body">
+                <h5 class="card-title">{item.productName}</h5>
+                {/* <p class="card-text">{item.detail}</p> */}
+              </div>
+              <div class="card-footer">
+                <input
+                  min="1"
+                  type="number"
+                  id="qty"
+                  name="qty"
+                  // value=
+                  // onChange=
+                />
+                <Button variant="primary" href={"/detailProduct/" + item._id}>
+                  Detail
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
-        </div>
-        
-        
-
       </div>
-    
-      
-    );
-  }
-  
-  export default Home;
-  
+    </div>
+  );
+}
+
+export default Home;
--- src/containers/companymanage.js
+++ src/containers/companymanage.js
@@ -1,21 +1,156 @@
 // import { Card, Button } from "react-bootstrap";
-import Nav from "../navbar/navbar"
-import '../styles/App.css';
+import React, { useState } from "react";
+import Nav from "../navbar/navbar";
+import styles from "./CartItem.module.css";
+import "../styles/App.css";
+import {
+  Card,
+  Button,
+  ButtonToolbar,
+  Row,
+  Navbar,
+  Col,
+  Modal,
+  Container,
+} from "react-bootstrap";
 
-function Home() {
-    return (
-      <div className="App">
-        <Nav/>
+
+
+function MydModalWithGrid(props) {
+  // const [picture, setPicture] = ([]);
+
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add New Product
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="show-grid">
+        <form>
+          <Container>
+            <Row>
+              <Col xs={12} md={8}>
+                <label>
+                  Product Name :<input type="text" name="Product Name"></input>
+                </label>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={7}>
+                <label>
+                  Price Unit : 
+                  <input type="text" name="Product Unit"></input>
+                </label>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={6} md={4}>
+                <label>
+                  Description : 
+                <textarea name='Description' />
+                </label>
+              </Col>
+              </Row>
+              <Row>
+              <Col xs={6} md={4}>
+                <label>
+                  Choose Image : 
+                  {/* <input type="file" onClick={() => setPicture()}></input> */}
+                  <input type="file"></input>
+                </label>
+                {/* .col-xs-6 .col-md-4 */}
+              </Col>
+              <Col xs={6} md={4}>
+                {/* .col-xs-6 .col-md-4 */}
+              </Col>
+            </Row>
+          </Container>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Submit</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function Companymanage() {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <div className="Companymanage">
+      <Nav />
+      <Navbar bg="light" expand="lg">
+        <div className="container">
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Add Product
+          </Button>
+
+          <MydModalWithGrid
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        </div>
+      </Navbar>
+      <Row>
         <div className="layoutPage">
           <div>
-            <p>แสดงตารางproductของเรา</p>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://storage.googleapis.com/file-computeandmore/images/051e9571-9e1f-45db-87d0-014a6d966b7c.jpg?fbclid=IwAR2tEfzpYhxJsLC2TxiXomNEjLIPR7awUYr2FOGOpXfY7R_OiOrI9glqb4w"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <div className={styles.cartItem__actions}>
+                  <div className={styles.cartItem__qty}>
+                    <label htmlFor="qty">Qty</label>
+                    <input
+                      min="1"
+                      type="number"
+                      id="qty"
+                      name="qty"
+                      // value={input}
+                      // onChange={onChangeHandler}
+                    />
+                  </div>
+                  <button
+                    // onClick={() => removeFromCart(item.id)}
+                    className={styles.actions__deleteItemBtn}
+                  >
+                    <img
+                      src="https://image.flaticon.com/icons/svg/709/709519.svg"
+                      alt=""
+                    />
+                  </button>
+                </div>
+              </Card.Body>
+            </Card>
+
             <p>เพิ่มproduct ได้</p>
-            <p>แก้ไขแต่ละproductได้/ลบ ประมาณhistoryในprofileก็ได้ หรือ ออกแบบใหม่เลย</p>
+            <p>
+              แก้ไขแต่ละproductได้/ลบ ประมาณhistoryในprofileก็ได้ หรือ
+              ออกแบบใหม่เลย
+            </p>
           </div>
-      </div>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+        </div>
+      </Row>
+    </div>
+  );
+}
+
+// ReactImageUploadComponent.propTypes = {
+//   buttonType: "button"
+// };
+
+// ReactImageUploadComponent.propTypes = {
+//   buttonType: PropTypes.string
+// };
+
+export default Companymanage;
