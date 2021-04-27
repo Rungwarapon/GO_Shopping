f0ad1723d059e587257fb1a1083e933622c5b56f
thitiwut088 <thitiwut00897@gmail.com>
2021-04-28 00:53:15 +0700

    create delete product search product
    <3

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,80 +1,66 @@
 import { Card, Button } from "react-bootstrap";
-<<<<<<< HEAD
-import Nav from "../navbar/navbar";
-import "../styles/App.css";
-import Axios from "axios";
-=======
 import Nav from "../navbar/navbar"
 // import Nav from "../navbar/navberv2"
 import '../styles/App.css';
 import Axios from 'axios';
->>>>>>> 8eabe23e092533cf0d66741add9d5ca4446cd763
 import React, { useState, useEffect } from "react";
+import { Form, FormControl} from 'react-bootstrap';
 
 function Home() {
   const [Product, SetProduct] = useState([]);
   const [ID, SetID] = useState();
+  const [searchValue, setSearchValue] = useState('');
+  const [searchProduct, setSearchProduct] = useState('');
   const fetchData = () => {
     Axios.get("http://localhost:3001/api/products").then((res) => {
       SetProduct(res.data);
     });
   };
 
-<<<<<<< HEAD
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div className="App">
-      <Nav />
-      <div className="container">
-        <h1 className="text-center font-italic">Product</h1>
-        <div class="card-columns">
-          {Product.map((item) => (
-            <div class="card">
-              <img
-                class="card-img-top"
-                src="/images/test.jpeg"
-                alt="Card image cap"
-                style={{ width: "100%", height: "Auto" }}
-              />
-              <div class="card-body">
-                <h5 class="card-title">{item.productName}</h5>
-                {/* <p class="card-text">{item.detail}</p> */}
-              </div>
-              <div class="card-footer">
-                <input
-                  min="1"
-                  type="number"
-                  id="qty"
-                  name="qty"
-                  // value=
-                  // onChange=
-                />
-                <Button variant="primary" href={"/detailProduct/" + item._id}>
-                  Detail
-                </Button>
-              </div>
-            </div>
-          ))}
-=======
-          })
-    }
     
 
     useEffect(() => {
       fetchData();
+
     }, []);
 
+
+  const handleChange = (event) => {
+    let fieldVal = event.target.value;
+    console.log(fieldVal);
+    setSearchProduct(fieldVal)
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let body = {
+      searchProduct: searchProduct
+    }
+    Axios.post('http://localhost:3001/api/index/search', body).then((response) => {
+      setSearchValue(response.data)
+      console.log(response.data)
+    })
+  }
     return (
       <div className="App">
         <Nav/>
         <div className="container">
+
+            <Form inline onSubmit={handleSubmit}>
+              <FormControl
+                type="text"
+                name="searchProduct"
+                placeholder="Search"
+                className="mr-sm-2"
+                value={searchProduct}
+                onChange={handleChange}
+              />
+              <Button variant="outline-success" type="submit">Search</Button>
+            </Form>
+
             <br/><h1 className="text-center font-italic">Product</h1><br/>
             <div className="order">
-              
-            { Product.map(item =>
+            {searchValue == '' ? Product.map(item =>
                 <div className='sizecard'>
                   <div className="card">
                     <img className="card-img-top" src={item.photos} alt="Card image cap" style={{ width: '100%', height: 'Auto'}}/>
@@ -89,13 +75,26 @@ function Home() {
                     </div>
                   </div>
                 </div>
-            )}
+            ): searchValue.map(item => 
+            <div className='sizecard'>
+              <div className="card">
+                    <img className="card-img-top" src={item.photos} alt="Card image cap" style={{ width: '100%', height: 'Auto'}}/>
+                    <div className="card-body">
+                      <h5 className="nameproductinhome">{item.productName}</h5>
+                    </div>
+                    <div className="card-footer col">
+                      <div className='pricebutton'>
+                        <Button className="hoverbutton" href={"/detailProduct/"+ item._id}>Detail</Button>
+                        <p className='spaceprice'><b>price {item.priceUnit}.-</b></p>
+                      </div>
+                    </div>
+                  </div>
+            </div> )}
             
             </div>
->>>>>>> 8eabe23e092533cf0d66741add9d5ca4446cd763
         </div>
       </div>
-    </div>
+
   );
 }
 
--- src/containers/companymanage.js
+++ src/containers/companymanage.js
@@ -1,23 +1,69 @@
 // import { Card, Button } from "react-bootstrap";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../navbar/navbar";
 import styles from "./CartItem.module.css";
 import "../styles/App.css";
-import {
-  Card,
-  Button,
-  ButtonToolbar,
-  Row,
-  Navbar,
-  Col,
-  Modal,
-  Container,
-} from "react-bootstrap";
+import {Card,Button,ButtonToolbar,Row,Navbar,Col,Modal,Container,} from "react-bootstrap";
+import Axios from 'axios';
+
 
 
 
 function MydModalWithGrid(props) {
-  // const [picture, setPicture] = ([]);
+  const [productName, setproductName] = useState();
+  const [priceUnit, setpriceUnit] = useState();
+  const [photos, setphotos] = useState();
+  const [detail, setdetail] = useState();
+  const [userId, setuserId] = useState();
+  
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let body = {
+      productName: productName,
+      priceUnit: priceUnit,
+      photos:photos,
+      detail:detail,
+      userId:userId
+    }
+    Axios.post('http://localhost:3001/api/products/create', body).then((response) => {
+      // setSearchValue(response.data)
+      setproductName('')
+      setpriceUnit('')
+      setphotos('')
+      setdetail('')
+      window.location.reload(true);
+    })
+    
+    // props.onHide()
+    // setTimeout(3000)
+    // window.location.reload(true);
+    
+  }
+
+  const handleChangeproductName = (event) => {
+    let fieldVal = event.target.value;
+    console.log(fieldVal);
+    setproductName(fieldVal)
+  }
+  const handleChangepriceUnit = (event) => {
+    let fieldVal = event.target.value;
+    console.log(fieldVal);
+    setpriceUnit(fieldVal)
+  }
+  const handleChangedetail = (event) => {
+    let fieldVal = event.target.value;
+    console.log(fieldVal);
+    setdetail(fieldVal)
+  }
+  const handleChangephotos = (event) => {
+    let fieldVal = event.target.value;
+    console.log(fieldVal);
+    setphotos(fieldVal)
+  }
+
+
 
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
@@ -26,19 +72,19 @@ function MydModalWithGrid(props) {
           Add New Product
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body className="show-grid">
-        <form>
+        <form onSubmit={handleSubmit}>
+        <Modal.Body className="show-grid">
           <Container>
             <Row>
               <Col xs={12} md={8}>
-                <label>
+                <label value={productName} onChange={handleChangeproductName}>
                   Product Name :<input type="text" name="Product Name"></input>
                 </label>
               </Col>
             </Row>
             <Row>
               <Col xs={12} md={7}>
-                <label>
+                <label value={priceUnit}onChange={handleChangepriceUnit}>
                   Price Unit : 
                   <input type="text" name="Product Unit"></input>
                 </label>
@@ -47,7 +93,7 @@ function MydModalWithGrid(props) {
 
             <Row>
               <Col xs={6} md={4}>
-                <label>
+                <label type="text" value={detail} onChange={handleChangedetail}>
                   Description : 
                 <textarea name='Description' />
                 </label>
@@ -55,34 +101,51 @@ function MydModalWithGrid(props) {
               </Row>
               <Row>
               <Col xs={6} md={4}>
-                <label>
+                <label value={photos} onChange={handleChangephotos}>
                   Choose Image : 
-                  {/* <input type="file" onClick={() => setPicture()}></input> */}
                   <input type="file"></input>
                 </label>
-                {/* .col-xs-6 .col-md-4 */}
               </Col>
               <Col xs={6} md={4}>
-                {/* .col-xs-6 .col-md-4 */}
               </Col>
             </Row>
           </Container>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-success" type="submit">Add</Button>
+          </Modal.Footer>
         </form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Submit</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
 
 function Companymanage() {
   const [modalShow, setModalShow] = useState(false);
+  const [Product, SetProduct] = useState([]);
+
+  const fetchData = () => {
+    Axios.get("http://localhost:3001/api/products").then((res) => {
+      SetProduct(res.data);
+    });
+  };
+
+    
+
+    useEffect(() => {
+      fetchData();
+      
+
+    }, []);
+
+  const handleDElete = (item) =>{
+    Axios.delete("http://localhost:3001/api/products/delete/" + item._id).then((res) => {});
+    window.location.reload(true);
+  }
 
   return (
-    <div className="Companymanage">
+    <div className="App">
       <Nav />
-      <Navbar bg="light" expand="lg">
         <div className="container">
           <Button variant="primary" onClick={() => setModalShow(true)}>
             Add Product
@@ -93,54 +156,32 @@ function Companymanage() {
             onHide={() => setModalShow(false)}
           />
         </div>
-      </Navbar>
-      <Row>
-        <div className="layoutPage">
-          <div>
-            <Card style={{ width: "18rem" }}>
+
+        <div className="order">
+          <div className='sizecard'>
+            {Product.map(item => <Card className='card'>
               <Card.Img
                 variant="top"
-                src="https://storage.googleapis.com/file-computeandmore/images/051e9571-9e1f-45db-87d0-014a6d966b7c.jpg?fbclid=IwAR2tEfzpYhxJsLC2TxiXomNEjLIPR7awUYr2FOGOpXfY7R_OiOrI9glqb4w"
+                src={item.photos}
               />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{item.productName}</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                {item.detail}
                 </Card.Text>
                 <div className={styles.cartItem__actions}>
                   <div className={styles.cartItem__qty}>
-                    <label htmlFor="qty">Qty</label>
-                    <input
-                      min="1"
-                      type="number"
-                      id="qty"
-                      name="qty"
-                      // value={input}
-                      // onChange={onChangeHandler}
-                    />
+                    <label htmlFor="qty">{item.priceUnit}</label>
                   </div>
-                  <button
-                    // onClick={() => removeFromCart(item.id)}
-                    className={styles.actions__deleteItemBtn}
-                  >
-                    <img
-                      src="https://image.flaticon.com/icons/svg/709/709519.svg"
-                      alt=""
-                    />
+                  <button className={styles.actions__deleteItemBtn} onClick={() => handleDElete(item)}>
+                    <img src="https://image.flaticon.com/icons/svg/709/709519.svg" alt=""/>
                   </button>
                 </div>
               </Card.Body>
-            </Card>
-
-            <p>เพิ่มproduct ได้</p>
-            <p>
-              แก้ไขแต่ละproductได้/ลบ ประมาณhistoryในprofileก็ได้ หรือ
-              ออกแบบใหม่เลย
-            </p>
+            </Card>)}
           </div>
         </div>
-      </Row>
+
     </div>
   );
 }
